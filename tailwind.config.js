980590219d2c3de6f0ef4f972e238b7c4bfd4c5e
DinhBang <ndinhbang111@gmail.com>
2022-03-11 22:52:17 +0700

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    content: [
+        './views/**/*.blade.php',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.yellow,
+                success: colors.green,
+                warning: colors.amber,
+            },
+            fontFamily: {
+                sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
